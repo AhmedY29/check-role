2f405d56f8b15af07b1b5d12c9b00b36ed6a0e2b
AhmedY29 <ahmedys2001@gmail.com>
2025-06-04 12:17:07 +0300

    complete 1

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,20 @@
-import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
+import {
+  createBrowserRouter,
+  Navigate,
+  Outlet,
+  RouterProvider,
+} from "react-router";
 import Home from "../pages/Home";
 import SignIn from "../pages/SignIn";
 import Navbar from "../components/Navbar";
 import SignUp from "../pages/SignUp";
 
 const Layout = () => {
+  let userInfo = JSON.parse(localStorage.getItem("UserName-Account"));
+
+  if (!userInfo) {
+    return <Navigate to={"/signin"} />;
+  }
   return (
     <>
       <Navbar />
